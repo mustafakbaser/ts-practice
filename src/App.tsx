a0decd2451cbce665c01b4c29ed1f806ef0a5b1e
Mustafa Kürşad BAŞER <mkursatbaser@gmail.com>
2024-03-11 23:48:16 +0300

    first commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,47 @@
-import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  let name: string = "Kürşad";
+  let age: number | string = 27; // String or Number
+  let activeUser: boolean = true;
+  let nonTypeVariable: any = "test";
+
+  // Creating object
+   /*let obj = {
+    name: "Kürşad",
+    age: 27,
+    activeUser: true
+  }*/
+
+  // Object with Type
+  type ObjTypes = {
+    name: string,
+    age?: number, // ? : Optional
+    activeUser: boolean
+  }
+
+   let obj: ObjTypes = {
+    name: "Kürşad",
+    age: 27,
+    activeUser: true
+  }
+
+  // Interface extends type
+  interface ObjInterface extends ObjTypes {
+    semester?: number
+  }
+
+  let objInterface: ObjInterface = {
+    name: "Kürşad",
+    age: 27,
+    activeUser: true,
+    semester: 2
+  }
+ 
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {name + ", " + age}
     </div>
   );
 }
